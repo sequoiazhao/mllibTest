e377d3ba57e7e10bf8a3298fbbdbede341730e92
zhaoming7 <zhaoming7@hisense.com>
2017-12-07 20:58:03 +0800

    对分词结果进行TF-IDF2

---
+++ src/main/com/mllibtest/LDAUtils.scala
@@ -0,0 +1,127 @@
+package com.mllibtest
+
+import org.apache.spark.Logging
+import org.apache.spark.ml.clustering.LDAModel
+import org.apache.spark.mllib.linalg.Vector
+import org.apache.spark.mllib.clustering.{EMLDAOptimizer, LDAOptimizer, OnlineLDAOptimizer}
+import org.apache.spark.rdd.RDD
+
+/**
+  * LDA算法工具类
+  *
+  * @author zhaoming on 2017-12-07 17:08
+  **/
+class LDAUtils(private var k: Int,
+               private var maxIterations: Int,
+               private var algorithm: String,
+               private var alpha: Double,
+               private var beta: Double,
+               private var checkpointInterval: Int,
+               private var checkpointDir: String
+              ) extends Logging with Serializable {
+
+  def this() = this(10, 20, "em", -1, -1, 10, "")
+
+  //set 和 get
+  def setK(k: Int): this.type = {
+    require(k > 0, "主题个数K必须大于0")
+    this.k = k
+    this
+  }
+
+  def setMaxIterations(maxIterations: Int): this.type = {
+    require(maxIterations > 0, "迭代次数必须大于0")
+    this.maxIterations = maxIterations
+    this
+  }
+
+  def setAlgorithm(algorithm: String): this.type = {
+    require(algorithm.equalsIgnoreCase("em") || algorithm.equalsIgnoreCase("online"), "参数估计算法只支持：em/online")
+    this.algorithm = algorithm
+    this
+  }
+
+  def setAlpha(alpha: Double): this.type = {
+    this.alpha = alpha
+    this
+  }
+
+  def setBeta(beta: Double): this.type = {
+    this.beta = beta
+    this
+  }
+
+  def setCheckpointInterval(checkpointInterval: Int): this.type = {
+    require(checkpointInterval > 0, "检查点间隔必须大于0")
+    this.checkpointInterval = checkpointInterval
+    this
+  }
+
+  def setCheckpointDir(checkpointDir: String): this.type = {
+    this.checkpointDir = checkpointDir
+    this
+  }
+
+  def getK: Int = this.k
+
+  def getMaxIterations: Int = this.maxIterations
+
+  def getAlgorithm: String = this.algorithm
+
+  def getAlpha: Double = this.alpha
+
+  def getBeta: Double = this.beta
+
+  def getCheckpointInterval: Int = checkpointInterval
+
+  def getCheckpointDir: String = this.checkpointDir
+
+  //选择算法em或Online
+
+  private def selectOptimizer(algorithm: String, corpusSize: Long): LDAOptimizer = {
+
+    val optimizer = algorithm.toLowerCase match {
+      case "em" => new EMLDAOptimizer
+      case "online" => new OnlineLDAOptimizer().setMiniBatchFraction(0.05 + 1.0 / corpusSize)
+      case _ => throw new IllegalArgumentException(
+        s"只支持：em和online算法，输入是：$algorithm.")
+    }
+    optimizer
+  }
+
+  def train(data:RDD[(Long,Vector)]):LDAModel={
+
+  }
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
